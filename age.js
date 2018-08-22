6e5b827adb2ed105219b104be356b9e308939d5a
joychakraborty0542 <40673527+joychakraborty0542@users.noreply.github.com>
2018-08-22 17:49:37 +0530

    Add files via upload

---
+++ age.js
@@ -0,0 +1,25 @@
+var age=prompt("what is your age")
+// If age is negative 
+if (age < 0) {
+ console.log("Come back once you're out of the womb");
+}
+// If age is 21 
+if (age === "21") {
+ console.log("Happy 21st Birthday!");
+}
+// If age is odd  
+//(not evenly divisible by two)  
+if (age % 2 !== 0) {
+ console.log("Your age is odd!");
+}
+
+if(age%Math.sqrt(age)===0)
+{
+console.log("your age is perfect square");
+}
+else {
+	console.log("your age is not perfect square");
+}
+
+
+
